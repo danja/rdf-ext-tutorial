442d6e608f3d889faaf014e5534964fcf67c6d1d
danja <danny.ayers@gmail.com>
2018-01-13 13:30:48 +0100

    starting

---
+++ src/Tasks.js
@@ -0,0 +1,32 @@
+/**
+ * Class representing a set of Tasks.
+ */
+class Tasks {
+
+  /**
+   * Get a string representation for the Tasks.
+   * @return {string} The string representation.
+   */
+  toString () {
+    // TODO
+  }
+}
+
+/**
+ * Class representing a single Task.
+ */
+class Task {
+
+  # Task
+  # description
+  # created
+  # completed
+  # status
+  # priority
+  # visibility
+
+
+  toString () {
+    // TODO
+  }
+}
